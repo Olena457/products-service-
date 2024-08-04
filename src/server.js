742573e6489f112580c85e37622ae438c2741123
wordisstuff <wordisstuff@gmail.com>
2024-08-04 03:35:11 -0500

    v1

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 
 import { env } from './utils/env.js';
+import errorHandler from './middlewares/errorHandler.js';
+import notFoundHandler from './middlewares/notFoundHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -11,11 +13,14 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
 
+  app.use('/', (req, res) => {
+    res.send('HELLO!!!');
+  });
   //   app.use(productsRouter);
 
-  //   app.use('*', notFoundHandler);
+  app.use('*', notFoundHandler);
 
-  //   app.use(errorHandler);
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
