c3a2554a8a01e7d20cad47d7ff75a3cfdc288f52
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-08-18 09:56:23 +0200

    fixx

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,10 +1,18 @@
 import { Router } from 'express';
-import {getAllProductsController, getProductByIdController } from '../controllers/products.js';
+import {
+  createProductController,
+  deleteProductController,
+  getAllProductsController,
+  getProductByIdController,
+  updateProductController,
+} from '../controllers/products.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
 router.get('/products', ctrlWrapper(getAllProductsController));
 router.get('/products/:productId', ctrlWrapper(getProductByIdController));
-
+router.post('/products', ctrlWrapper(createProductController));
+router.delete('products/:productId', ctrlWrapper(deleteProductController));
+router.patch('/products/:productId', ctrlWrapper(updateProductController));
 export default router;
--- src/services/products.js
+++ src/services/products.js
@@ -5,5 +5,20 @@ export async function getAllProductsService() {
 }
 
 export async function getProductByIdService(productId) {
-  return await Product.findOne({ _id:productId });
+  return await Product.findOne({ _id: productId });
+}
+
+export function createProduct(payload) {
+  return Product.create(payload);
+}
+
+export function updateProduct(productId, updateData) {
+  return Product.findByIdAndUpdate(productId, updateData, {
+    new: true,
+    runValidators: true,
+  });
+}
+
+export function deleteProduct(productId) {
+  return Product.findByIdAndDelete(productId);
 }
